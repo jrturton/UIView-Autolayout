9e5ad51284c8b0504927373ccffc14b1e5558c5f
James Frost <james@jamesfrost.co.uk>
2014-05-20 07:48:14 +0100

    Added unit testing target.

---
+++ Example/AutoLayoutTests/AutoLayoutTests.m
@@ -0,0 +1,34 @@
+//
+//  AutoLayoutTests.m
+//  AutoLayoutTests
+//
+//  Created by James Frost on 20/05/2014.
+//  Copyright (c) 2014 James Frost. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AutoLayoutTests : XCTestCase
+
+@end
+
+@implementation AutoLayoutTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
